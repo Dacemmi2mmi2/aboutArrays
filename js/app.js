0f97c5f31d9f9971226114bf64f6d638255ff27a
Dacemmi2mmi2 <46452261+Dacemmi2mmi2@users.noreply.github.com>
2021-02-01 10:42:12 +0200

    added switch for bloks of content

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 const htmlElements = {
-    itemNavigation : document.querySelector('.itemNavigation'),
+    itemsNavigation : document.querySelector('.itemNavigation'),
+    itemNav : document.querySelectorAll('.nav'),
+    itemText : document.querySelectorAll('.textContent'),
 }
 
 
-htmlElements.itemNavigation.addEventListener('click', event => {
+const positionContent = function positionBlockscontent(paramCont)
+{
+    htmlElements.itemText.forEach(item => {
+        item.style.left = 150 + '%';
+        if(item.classList[0].slice(12) === paramCont.slice(5))
+        {
+            item.style.display = 'block';
+            show(item);
+        }
+    });
+
+    function show (element)
+    {
+        setTimeout(() => {
+            element.style.left = 0;
+            htmlElements.itemText.forEach(el => {
+                el != element ? el.style.display = 'none' : '';
+            });
+        }, 600);
+    }
+}
+
+
+const positionNav = function positionBlocksNavigation(paramNav)
+{
+    htmlElements.itemNav.forEach(item => {
+        item.style.left = -150 + '%';
+        item.classList[0].slice(10) === paramNav.slice(5) ? item.style.left = 0 : '';
+    });
+}
+
+
+htmlElements.itemsNavigation.addEventListener('click', event => {
     if (event.target.closest('div.itemNavigation'))
     {
-        
+        positionContent(event.target.id);
+        positionNav(event.target.id);
     }
 });
